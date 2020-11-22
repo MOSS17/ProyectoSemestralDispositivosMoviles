c81ae99536f3a345112f59f7f47569cfd6e534f9
Fer <reikafer@gmail.com>
2020-11-22 14:28:25 -0700

    Initial Commit

---
+++ ProyectoSemestralMiguelRubén/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoSemestralMiguelRubén
+//
+//  Created by Alumno on 11/22/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
